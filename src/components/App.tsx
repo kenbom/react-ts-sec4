4c4d3ada5024cf357309bdb9f6ff3391b9a027ad
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-12-21 21:17:52 +0000

    add redux files

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from '../state'
+import RepositoriesList from './RepositoriesList'
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <div><h1>Search NPM Package</h1></div>
+            <RepositoriesList />
+        </Provider>
+    )
+}
+
+export default App
---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RepositoriesList: React.VFC = () => {
+    return (
+        <div>
+            <form action=""><input></input>
+            <button>
+                </button>
+                </form>
+        </div>
+    )
+}
+
+export default RepositoriesList
